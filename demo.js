1893c37c67912847c56c55d0dc4ff77e9f80e41d
Nghia Nguyen <nghia.nguyen@saigontechnology.com>
2021-03-04 09:53:43 +0700

    Initial commit

---
+++ demo.js
@@ -0,0 +1,24 @@
+var http = require("http"),
+  fs = require("fs"),
+  url = require("url");
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true); // Show Difference Pages
+    var fileName = "." + q.pathname;
+    if (fileName === "./") {
+      fileName = "./index";
+    }
+    fileName = fileName + ".html";
+    fs.readFile(fileName, function (err, data) {
+      // File System and Index Page
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
